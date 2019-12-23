f8ce891a47651ef5ab8155b448cdb9656ea95199
hiimjako <moretti919@gmail.com>
2019-12-24 00:51:32 +0100

    [fix] compiler

--- compileC.sh
+++ compileC.sh
@@ -2,4 +2,4 @@
 #bash compileC.sh "name"
 riscv64-unknown-elf-gcc -o $1 $1.s
 qemu-riscv64 $1
-rm -r *
+rm -r $1
--- main.s
+++ main.s
@@ -29,7 +29,7 @@
 
 #-----------global symbol-----------#
 .section .data
-    array: .double 10, 15, 21, 23, 27, 31, 39, 40, 47, 50
+    array: .byte 10, 15, 21, 23, 27, 31, 39, 40, 47, 50
     num: .byte 27
     len: .byte 10
 
@@ -39,17 +39,39 @@
 #-----------#
 #a0 = v
 #a1 = n
+fun:
+    li t1, 0
+    lb t2, 0(a0)
+    mv t4, a0 #partenza
+    mv t3, a0 #index []
+    la a0, msg
 l1:
+    bge t1, a1, el1	
+    # beq , , ret	# i  == $t1 then targe
+    mv a1, t2
+    jal ra, printf
+    
+    addi t1, t1, 1
+    # add t3, t4, t1
+    # lb t2, 0(t3)
+    beq zero, zero, l1
 
+el1:
+    jalr zero, ra
 
 #Codice assembly
 .section .text
 main:
 	la a0, msgStart
-    li a1, 30
+    la t1, num
+    lb a1, 0(t1)
     jal ra, printf
 
-  	
+    la a0, array
+    la t1, len
+    lb a1, 0(t1)
+    jal ra, fun
+      
 _end:
     li a7, _SYS_EX
     ecall

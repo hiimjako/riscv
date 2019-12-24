141be782443a8bed1d097b58a34ffd376ffe6dac
hiimjako <moretti919@gmail.com>
2019-12-24 02:06:57 +0100

    [end] fine creazione della ricerca sequenziale

--- main.s
+++ main.s
@@ -25,53 +25,69 @@
 
 .section .rodata
     msgStart: .string "Il numero da trovare e`: %d\n" 
-	msg: .string "%d\n"
+    msgPreStart: .string "numero da cercare: %d, len di array %d\n"
+    msgend: .string "Il numero trovato e' in posizione: %d\n"
+    msgerr: .string "Numero non presente nell' array\n"
 
 #-----------global symbol-----------#
 .section .data
-    array: .byte 10, 15, 21, 23, 27, 31, 39, 40, 47, 50
-    num: .byte 27
-    len: .byte 10
+    array: .byte 11, 15, 21, 23, 27, 31, 39, 40, 47, 50
+    num: .byte 22
+    len: .byte 9
 
 #-----------code-----------#
 .section .text
 
 #-----------#
-#a0 = v
-#a1 = n
+#a0 = array
+#a1 = numero da cercare
+#a2 = len array
 fun:
-    li t1, 0
-    lb t2, 0(a0)
-    mv t4, a0 #partenza
-    mv t3, a0 #index []
-    la a0, msg
+    li t1, 0 #i
+    lb t2, 0(a0) #a[i]
 l1:
-    bge t1, a1, el1	
-    # beq , , ret	# i  == $t1 then targe
-    mv a1, t2
-    jal ra, printf
-    
+    bge t1, a2, el1	
+    beq t2, a1, el2
     addi t1, t1, 1
-    # add t3, t4, t1
-    # lb t2, 0(t3)
+    add t3, a0, t1
+    lb t2, 0(t3)
     beq zero, zero, l1
-
 el1:
+    li a0, -1
+    jalr zero, ra 
+el2:
+    mv a0, t1
     jalr zero, ra
 
 #Codice assembly
 .section .text
 main:
-	la a0, msgStart
+
+    la a0, msgPreStart
     la t1, num
     lb a1, 0(t1)
+    la t2, len
+    lb a2, 0(t2)
+    jal ra, printf 
+	
+    #chiamo la funzione
+    la a0, array
+    la t1, num
+    lb a1, 0(t1)
+    la t2, len
+    lb a2, 0(t2)
+    jal ra, fun
+    
+    #fine funzione, stampo risultato
+    li t1, -1
+    beq a0, t1, else
+    addi a1, a0, 0
+    la a0, msgend
+    jal ra, printf
+else:
+    la a0, msgerr
     jal ra, printf
 
-    la a0, array
-    la t1, len
-    lb a1, 0(t1)
-    jal ra, fun
-      
 _end:
     li a7, _SYS_EX
     ecall
